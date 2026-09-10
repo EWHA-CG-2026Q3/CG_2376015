3144a9a7682e2a70859567f341ea483d3fdf904e
sungeun <ksekse7147@gamil.com>
2026-09-10 10:48:21 +0900

    아래쪽 완성

--- Assets/S04/S04_CustomDiamondMesh.cs
+++ Assets/S04/S04_CustomDiamondMesh.cs
@@ -30,10 +30,8 @@ public class S04_CustomDiamondMesh : MonoBehaviour
             // 아래쪽 4면 (정점 5 사용)
             0,1,5,
             1,2,5,
-            //2,3,5,
-            //3,0,5
-            
- 
+            2,3,5,
+            3,0,5
         };
 
         Mesh mesh = new Mesh();
